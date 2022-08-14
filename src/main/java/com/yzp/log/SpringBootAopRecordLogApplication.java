88ae0004683dbb48f73ce9ab58b202b7774493c7
yzp <2306807556@qq.com>
2022-08-14 19:03:01 +0800

    init

---
+++ src/main/java/com/yzp/log/SpringBootAopRecordLogApplication.java
@@ -0,0 +1,13 @@
+package com.yzp.log;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAopRecordLogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAopRecordLogApplication.class, args);
+    }
+
+}
